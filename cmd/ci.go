3123a306440c34eb64e5de007e61e1a332619497
t-monaghan <tomaghan@gmail.com>
2025-06-13 22:00:23 +0900

    changes behaviour to continue polling until checks are complete

--- cmd/ci.go
+++ cmd/ci.go
@@ -5,15 +5,20 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/cli/go-gh/v2"
 	"github.com/spf13/cobra"
 	"github.com/t-monaghan/altar/examples/pipelinewatcher"
 )
 
-var brokerAddress string //nolint:gochecknoglobals
+var brokerAddress string
+
+const loopDelay = time.Second * 5
 
 func init() {
 	rootCmd.AddCommand(ciCmd)
@@ -29,34 +34,43 @@ var ciCmd = &cobra.Command{
 }
 
 func ci(_ *cobra.Command, _ []string) {
-	out, stderr, err := gh.Exec("pr", "checks", "--json", "name,bucket")
-	if err != nil {
-		slog.Error("error encountered querying gh cli for pr check information", "error", err, "stderr", stderr.String())
+	var failedActions []string
 
-		return
+	completedActions := -1
+	totalActions := 0
+
+	for completedActions != totalActions {
+		response, err := queryGH()
+		if err != nil {
+			slog.Error("failed to query github", "error", err)
+
+			return
+		}
+
+		totalActions, completedActions, failedActions = getCheckInfo(response)
+		status := pipelinewatcher.PullRequestActionsStatus{
+			CompletedActions: completedActions,
+			TotalActions:     totalActions,
+			FailedActions:    failedActions,
+		}
+
+		err = sendRequest(status)
+		if err != nil {
+			slog.Error("error sending request to altar admin", "error", err)
+
+			return
+		}
+
+		time.Sleep(loopDelay)
 	}
 
-	var response []ChecksResult
-	err = json.Unmarshal(out.Bytes(), &response)
+	slog.Info("checks complete")
+}
 
-	if err != nil {
-		slog.Error("failed to unmarshal github cli response into StatusCheckGroup", "error", err)
-
-		return
-	}
-
-	totalActions, completedActions, failedActions := getCheckInfo(response)
-	status := pipelinewatcher.PullRequestActionsStatus{
-		CompletedActions: completedActions,
-		TotalActions:     totalActions,
-		FailedActions:    failedActions,
-	}
-
+func sendRequest(status pipelinewatcher.PullRequestActionsStatus) error {
 	jsonData, err := json.Marshal(status)
 	if err != nil {
-		slog.Error("failed to marshal github pr status into json", "error", err)
-
-		return
+		return fmt.Errorf("failed to marshal json data into PullRequestActionStatus: %w", err)
 	}
 
 	bufferedJSON := bytes.NewBuffer(jsonData)
@@ -65,22 +79,16 @@ func ci(_ *cobra.Command, _ []string) {
 	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, brokerAddress, bufferedJSON)
 
 	if err != nil {
-		slog.Error("failed to marshal github pr status into json", "error", err)
-
-		return
+		return fmt.Errorf("failed to marshal github pr status into json: %w", err)
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		slog.Error("failed to perform request to altar admin", "error", err)
-
-		return
+		return fmt.Errorf("failed to perform request to altar admin: %w", err)
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		slog.Error("altar admin responded to request with non-200 status", "status", resp.Status)
-
-		return
+		return fmt.Errorf("altar admin responded to request with non-200 status: %v", resp.Status) //nolint:err113
 	}
 
 	defer func() {
@@ -89,6 +97,27 @@ func ci(_ *cobra.Command, _ []string) {
 			slog.Error("failed to close body of github state request", "error", closeErr)
 		}
 	}()
+
+	return nil
+}
+
+// ErrGHCLIQuery represents an error querying GitHub CLI.
+var ErrGHCLIQuery = errors.New("error encountered querying gh cli for pr check information")
+
+func queryGH() ([]ChecksResult, error) {
+	out, stderr, err := gh.Exec("pr", "checks", "--json", "name,bucket")
+	if err != nil {
+		return nil, fmt.Errorf("%w: %v", ErrGHCLIQuery, stderr.String())
+	}
+
+	var response []ChecksResult
+	err = json.Unmarshal(out.Bytes(), &response)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal github cli response into StatusCheckGroup: %w", err)
+	}
+
+	return response, nil
 }
 
 func getCheckInfo(response []ChecksResult) (int, int, []string) {
@@ -102,6 +131,7 @@ func getCheckInfo(response []ChecksResult) (int, int, []string) {
 			completedActions++
 		case "fail", "skipping", "cancel":
 			failedActions = append(failedActions, action.Name)
+			completedActions++
 		}
 	}
 
